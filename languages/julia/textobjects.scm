778e8e35d9033e278ee07870e66e44a7ca1611d9
yushengzhao <yushengzhao2020@outlook.com>
2025-10-24 20:56:46 +0800

    add textobjects.scm

---
+++ languages/julia/textobjects.scm
@@ -0,0 +1,9 @@
+(function_definition) @function.around
+
+(macro_definition) @function.around
+
+(struct_definition) @class.around
+
+(line_comment)+ @comment.around
+
+(block_comment) @comment.around
